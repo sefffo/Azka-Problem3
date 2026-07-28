ea237edc1b6bceccde3810aa2f512f12e56a29db
Saif Lotfy <154231473+sefffo@users.noreply.github.com>
2026-07-29 00:39:28 +0300

    fix: add missing Email property to Engineer entity and related files
    - Engineer.cs: add Email property
    - CreateEngineerDto, UpdateEngineerDto, EngineerDto: add Email field
    - EngineerService: map Email in CreateAsync, UpdateAsync, MapToDto
    - Migration AddEmailToEngineer: add Email column to Engineers table

--- Azka.Services/DTOs/Engineer/CreateEngineerDto.cs
+++ Azka.Services/DTOs/Engineer/CreateEngineerDto.cs
@@ -1,12 +1,29 @@
+using FluentValidation;
+
 namespace Azka.Services.DTOs.Engineer;
 
 public class CreateEngineerDto
 {
-    public string EmployeeNumber { get; set; } = string.Empty;
-    public string FullName { get; set; } = string.Empty;
-    public string Team { get; set; } = string.Empty;
-    public string Region { get; set; } = string.Empty;
-    public string Skills { get; set; } = string.Empty;
-    public string WorkingHours { get; set; } = "08:00-16:00";
+    public string EmployeeNumber     { get; set; } = string.Empty;
+    public string FullName           { get; set; } = string.Empty;
+    public string Email              { get; set; } = string.Empty;
+    public string Team               { get; set; } = string.Empty;
+    public string Region             { get; set; } = string.Empty;
+    public string Skills             { get; set; } = string.Empty;
+    public string WorkingHours       { get; set; } = "08:00-16:00";
     public double DailyCapacityHours { get; set; } = 8.0;
+}
+
+public class CreateEngineerDtoValidator : AbstractValidator<CreateEngineerDto>
+{
+    public CreateEngineerDtoValidator()
+    {
+        RuleFor(x => x.EmployeeNumber).NotEmpty();
+        RuleFor(x => x.FullName).NotEmpty();
+        RuleFor(x => x.Email).EmailAddress().When(x => !string.IsNullOrWhiteSpace(x.Email));
+        RuleFor(x => x.Team).NotEmpty();
+        RuleFor(x => x.Region).NotEmpty();
+        RuleFor(x => x.WorkingHours).NotEmpty();
+        RuleFor(x => x.DailyCapacityHours).GreaterThan(0);
+    }
 }
--- Azka.Services/DTOs/Engineer/UpdateEngineerDto.cs
+++ Azka.Services/DTOs/Engineer/UpdateEngineerDto.cs
@@ -1,12 +1,28 @@
+using FluentValidation;
+
 namespace Azka.Services.DTOs.Engineer;
 
 public class UpdateEngineerDto
 {
-    public string FullName { get; set; } = string.Empty;
-    public string Team { get; set; } = string.Empty;
-    public string Region { get; set; } = string.Empty;
-    public string Skills { get; set; } = string.Empty;
-    public string WorkingHours { get; set; } = "08:00-16:00";
+    public string FullName           { get; set; } = string.Empty;
+    public string Email              { get; set; } = string.Empty;
+    public string Team               { get; set; } = string.Empty;
+    public string Region             { get; set; } = string.Empty;
+    public string Skills             { get; set; } = string.Empty;
+    public string WorkingHours       { get; set; } = "08:00-16:00";
     public double DailyCapacityHours { get; set; } = 8.0;
-    public bool IsActive { get; set; } = true;
+    public bool   IsActive           { get; set; } = true;
+}
+
+public class UpdateEngineerDtoValidator : AbstractValidator<UpdateEngineerDto>
+{
+    public UpdateEngineerDtoValidator()
+    {
+        RuleFor(x => x.FullName).NotEmpty();
+        RuleFor(x => x.Email).EmailAddress().When(x => !string.IsNullOrWhiteSpace(x.Email));
+        RuleFor(x => x.Team).NotEmpty();
+        RuleFor(x => x.Region).NotEmpty();
+        RuleFor(x => x.WorkingHours).NotEmpty();
+        RuleFor(x => x.DailyCapacityHours).GreaterThan(0);
+    }
 }
