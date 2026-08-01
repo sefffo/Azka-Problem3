eeb17d1bf8bb13772314bc9de49a1be531b0a8b7
Youssef <youssefabdelmoniem11@gmail.com>
2026-08-01 15:31:24 +0300

    fix(db): regenerate AddEmailToEngineer migration to fix invalid column error

---
+++ Azka.Persistence/Migrations/20260801120615_AddEmailToEngineer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Azka.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailToEngineer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Engineers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Engineers");
+        }
+    }
+}
